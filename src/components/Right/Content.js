b96cb504f8c8bfff738bcbe84efd6e41f811d775
suno97 <ssh9234@gmail.com>
2022-12-25 20:35:06 +0900

    serachbar

--- src/components/Right/Content.js
+++ src/components/Right/Content.js
@@ -1,11 +1,17 @@
+import SearchBar from './SearchBar';
 
 const Content = ()=>{
   return(
     <div style={{
+      paddingTop:'13px',
+      paddingLeft:'30px',
+      paddingRight:'20px',
       backgroundColor:'orange',
       height:'100%',
+      overflow:'hidden',
     }}>
-      content
+      <SearchBar/>
+      <div>온라인-11명</div>
     </div>
   )
 }
---
+++ src/components/Right/SearchBar.js
@@ -0,0 +1,53 @@
+import searchIcon from '../../imgs/search.png';
+
+const SearchBar = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      flex: '1',
+      height: '32px',
+      width: '100%',
+      backgroundColor: '#202225',
+      alignItems: 'center',
+      borderRadius: '4px 4px 4px 4px / 4px 4px 4px 4px',
+      minWidth: '0',
+    }}>
+      <input style={{
+        flexGrow: '1',
+        flexShrink: '1',
+        minWidth: '0',
+        maxWidth: '100%',
+        width: '10px',
+        fontSize: '16px',
+        lineHeight: '32px',
+        height: '30px',
+        backgroundColor: '#202225',
+        border:'none',
+        padding:'0 8px',
+        borderRadius: '4px 4px 4px 4px / 4px 4px 4px 4px',
+        fontWeight:'bold',
+        boxSizing: 'border-box',
+        color: 'gray',
+      }} placeholder='검색하기'>
+      </input>
+      <div style={{
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        flexShrink: '0',
+        width: '32px',
+        height: '20px',
+        borderRadius: '4px 4px 4px 4px / 4px 4px 4px 4px',
+      }}>
+        <img src={searchIcon} alt='searchIcon' style={{
+          height:'16px',
+        }} />
+      </div>
+
+    </div>
+
+
+
+  )
+}
+export default SearchBar;
