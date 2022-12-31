3b664b1bd94a1201a67b99c8db2390453037dd52
suno97 <ssh9234@gmail.com>
2022-12-31 14:06:26 +0900

    loginform

--- src/components/LogInForm/LoginForm.js
+++ src/components/LogInForm/LoginForm.js
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { selectId, loginRequest } from '../../redux/reducers/user';
+import ReturnIcon from '../../imgs/returnico.svg';
 
 const LoginForm = () => {
   const [hoverPasswordMissing, setHoverPasswordMissing] = useState(false);
   const [hoverLogin, setHoverLogin] = useState(false);
+  const [hoverSignUp, setHoverSignUp] = useState(false);
+  const [hoverReturn, setHoverReturn] = useState(false);
 
   const dispatch = useDispatch();
 
   const onClickReturn = () => {
     dispatch(selectId());
+  }
+  const onMouseEnterReturn = () => {
+    setHoverReturn(true);
+  }
+  const onMouseLeaveReturn = () => {
+    setHoverReturn(false);
   }
 
   const onMouseEnterPasswordMissing = () => {
@@ -29,6 +38,19 @@ const LoginForm = () => {
     setHoverLogin(false);
   }
 
+  const onClickSignUp = () => {
+    dispatch(loginRequest());
+  }
+  const onMouseEnterSignUp = () => {
+    setHoverSignUp(true);
+  }
+  const onMouseLeaveSignUp = () => {
+    setHoverSignUp(false);
+  }
+
+
+
+
   return (
     <div style={{
       display: 'flex',
@@ -42,14 +64,61 @@ const LoginForm = () => {
       alignItems: 'start',
       boxSizing: 'border-box',
     }}>
-
-      <button style={{
-        padding: '2px 4px',
-        cursor: 'pointer',
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        marginBottom: '16px',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
-        onClick={onClickReturn}>
-        <div>뒤로 가기</div>
-      </button>
+
+      >
+        <button style={{
+          flexShrink: '0',
+          display: 'flex',
+          alignItems: 'center',
+          cursor: 'pointer',
+          background: 'none',
+          border: 'none',
+
+        }}
+          onClick={onClickReturn}
+          onMouseEnter={onMouseEnterReturn}
+          onMouseLeave={onMouseLeaveReturn}
+        >
+          <img src={ReturnIcon} alt="returnIcon" style={{
+            width: '16px',
+            height: '16px',
+            color: 'white',
+          }} />
+
+          <div style={{
+            background: 'none',
+            border: 'none',
+            padding: '2px 4px',
+            cursor: 'pointer',
+            color: '#dcddde',
+          }}
+          >
+            <div style={{
+              fontSize: '16px',
+              lineHeight: '20px',
+              fontWeight: '400',
+            }}>
+              뒤로 가기
+            </div>
+          </div>
+        </button>
+        <div style={{
+          height: '1px',
+          backgroundColor: hoverReturn ? 'white' : 'transparent',
+          width: '90%',
+          marginTop: '-5px',
+        }}>
+
+        </div>
+      </div>
+
 
       <div style={{
         width: '100%',
@@ -83,13 +152,90 @@ const LoginForm = () => {
         width: '100%',
         marginTop: '20px',
       }}>
-        <div className="emailorphone">
-          <label>이메일 또는 전화번호</label>
-          <div>as39as@naver.com</div>
+        <div className="emailorphone" style={{
+          marginBottom: '20px',
+        }}>
+          <label style={{
+            display: 'block',
+            fontSize: '12px',
+            lineHeight: '16px',
+            color: '#b9bbbe',
+            marginBottom: '8px',
+            fontWeight: '700',
+
+          }}>
+            이메일 또는 전화번호
+            <span style={{
+              color: '#ed4245',
+              paddingLeft: '4px',
+              lineHeight: '16px',
+            }}>
+              *
+            </span>
+          </label>
+          <div style={{
+            display: 'flex',
+            width: '100%',
+            borderRadius: '3px',
+            border: 'none',
+            boxSizing: 'border-box',
+            fontSize: '16px',
+          }}>
+            <input style={{
+              border: 'none',
+              padding: '10px',
+              height: '40px',
+              width: '100%',
+              boxSizing: 'border-box',
+              borderRadius: '3px',
+              backgroundColor: '#e8f0fe'
+            }}
+              placeholder=""
+              autoCorrect="off"
+              spellCheck="false"
+              maxLength={999}
+              required
+              type="text"
+            >
+            </input>
+          </div>
         </div>
         <div className="password">
-          <label>비밀번호</label>
-          <div>*************</div>
+          <label style={{
+            display: 'block',
+            fontSize: '12px',
+            lineHeight: '16px',
+            color: '#b9bbbe',
+            marginBottom: '8px',
+            fontWeight: '700',
+
+          }}>
+            비밀번호
+            <span style={{
+              color: '#ed4245',
+              paddingLeft: '4px',
+              lineHeight: '16px',
+            }}>
+              *
+            </span>
+          </label>
+          <input style={{
+            border: 'none',
+            padding: '10px',
+            height: '40px',
+            width: '100%',
+            boxSizing: 'border-box',
+            borderRadius: '3px',
+            backgroundColor: '#e8f0fe'
+          }}
+            placeholder=""
+            autoCorrect="off"
+            spellCheck="false"
+            maxLength={999}
+            required
+            type="password"
+          >
+          </input>
         </div>
       </div>
       <button style={{
@@ -132,7 +278,38 @@ const LoginForm = () => {
         onClick={onClickLogin}>
         로그인
       </button>
-      <button>계정이 필요한가요? 가입하기</button>
+      <div style={{
+        marginTop: '4px',
+      }}>
+        <span style={{
+          fontSize: '14px',
+          lineHeight: '16px',
+          color: '#a3a6aa',
+        }}>
+          계정이 필요한가요?
+        </span>
+        <button style={{
+          color: '#00aff4',
+          margin: '0 auto',
+          textOverflow: 'ellipsis',
+          overflow: 'hidden',
+          marginLeft: '4px',
+          verticalAlign: 'bottom',
+          background: 'none',
+          border: 'none',
+          fontWeight: '500',
+          lineHeight: '16px',
+          fontSize: '14px',
+          textDecoration: hoverSignUp ? 'underline' : 'none',
+          cursor: 'pointer',
+        }}
+          onMouseEnter={onMouseEnterSignUp}
+          onMouseLeave={onMouseLeaveSignUp}
+          onClick={onClickSignUp}
+        >
+          가입하기
+        </button>
+      </div>
     </div>
   )
 }
